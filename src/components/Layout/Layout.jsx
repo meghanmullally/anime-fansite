d71cd38cfd2dc89ba7d2160e45033ece9115b71c
meghanmullally <ms.mullally27@gmail.com>
2024-12-17 19:57:04 -0800

    remove /public

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -44,7 +44,7 @@ const Layout = ({ children }) => {
           <div
             className="toolbar-icon"
             style={{
-              backgroundImage: `url('/public/assets/windows95_icons/home.png')`,
+              backgroundImage: `url('/assets/windows95_icons/home.png')`,
             }}
           ></div>
           Home
@@ -58,7 +58,7 @@ const Layout = ({ children }) => {
       <div className="content">{children}</div>
       <div className="status-bar">
         <span className="status-text"><img src="public/assets/Ico/Webpage.ico" className="status-icon-img"/>Done</span>
-        <span className="status-icon"><img className="status-icon-img" src="/public/assets/windows95_icons/Earth (16 colors).ico"/>Internet</span>
+        <span className="status-icon"><img className="status-icon-img" src="/assets/windows95_icons/Earth (16 colors).ico"/>Internet</span>
       </div>
     </div>
   );
